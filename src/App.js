d809911088472deb49e47b47cee552e3c01ac5ed
HungNguyen-89 <hungnguyenhuts89@gmail.com>
2023-02-20 20:42:57 +0700

    video #48

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.scss";
+import Header from "./components/Header/Header";
+import { Link } from "react-router-dom";
 
 const App = () => {
-  return <div className="app-container">Hello world</div>;
+  return (
+    <div className="app-container">
+      <Header />
+      <div>
+        test link
+        <div>
+          <button>
+            <Link to="/users">Go to User page</Link>
+          </button>
+          <button>
+            <Link to="/admins">Go to Admin page</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
