ba1feb19d77c8d95c23c21dc410b321e07b8011d
HungNguyen-89 <hungnguyenhuts89@gmail.com>
2023-02-23 00:31:13 +0700

    video#58

---
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -0,0 +1,151 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { FcPlus } from "react-icons/fc";
+import axios from "axios";
+
+const ModalCreateUser = (props) => {
+  const { show, setShow } = props;
+  const handleClose = () => {
+    setShow(false);
+    setEmail("");
+    setPassword("");
+    setUsername("");
+    setRole("USER");
+    setImage("");
+    setPreviewImage("");
+  };
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [role, setRole] = useState("USER");
+  const [image, setImage] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
+
+  const handleUploadImage = (e) => {
+    if (e.target && e.target.files && e.target.files[0]) {
+      setPreviewImage(URL.createObjectURL(e.target.files[0]));
+      setImage(e.target.files[0]);
+    } else {
+    }
+  };
+
+  const handleSubmitCreateUser = async () => {
+    // let data = {
+    //   email: email,
+    //   password: password,
+    //   username: username,
+    //   role: role,
+    //   userImage: image,
+    // };
+    // console.log(data);
+
+    const data = new FormData();
+    data.append("email", email);
+    data.append("password", password);
+    data.append("username", username);
+    data.append("role", role);
+    data.append("userImage", image);
+
+    let res = await axios.post(
+      "http://localhost:8081/api/v1/participant",
+      data
+    );
+
+    console.log(">>> check res: ", res);
+  };
+
+  return (
+    <>
+      {/* <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button> */}
+
+      <Modal
+        backdrop="static"
+        show={show}
+        onHide={handleClose}
+        size="xl"
+        className="modal-add-user"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add new user</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form className="row g-3">
+            <div className="col-md-6">
+              <label className="form-label">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                id="inputEmail4"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Username</label>
+              <input
+                type="text"
+                className="form-control"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="col-md-4">
+              <label className="form-label">Role</label>
+              <select
+                className="form-select"
+                onChange={(e) => setRole(e.target.value)}
+              >
+                <option selected value="USER">
+                  USER
+                </option>
+                <option value="ADMIN">ADMIN</option>
+              </select>
+            </div>
+            <div className="col-md-12">
+              <label className="form-label label-upload" htmlFor="labelUpload">
+                <FcPlus />
+                Upload File Image
+              </label>
+              <input
+                type="file"
+                id="labelUpload"
+                hidden
+                onChange={(e) => handleUploadImage(e)}
+              />
+            </div>
+            <div className="col-md-12 img-preview">
+              {previewImage ? (
+                <img src={previewImage} />
+              ) : (
+                <span>Preview Image</span>
+              )}
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => handleSubmitCreateUser()}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalCreateUser;
