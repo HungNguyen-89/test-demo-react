709915b2eb01fa81306c844b2d19f00c9c84ac5e
HungNguyen-89 <hungnguyenhuts89@gmail.com>
2023-02-28 22:29:30 +0700

    video#70

---
+++ src/Layout.js
@@ -0,0 +1,43 @@
+import App from "./App";
+import User from "./components/User/User";
+import Admin from "./components/Admin/Admin";
+import HomePage from "./components/Home/HomePage";
+import DashBoard from "./components/Admin/Content/DashBoard";
+import ManageUser from "./components/Admin/Content/ManageUser";
+import Login from "./components/Admin/Auth/Login";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+const Layout = (props) => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="users" element={<User />} />
+        </Route>
+        <Route path="/admins" element={<Admin />}>
+          <Route index element={<DashBoard />} />
+          <Route path="manage-users" element={<ManageUser />} />
+        </Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
+};
+
+export default Layout;
--- src/components/Admin/Auth/Login.js
+++ src/components/Admin/Auth/Login.js
@@ -1,14 +1,31 @@
 import { useState } from "react";
 import "./Login.scss";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { postLogin } from "../../../services/apiServices";
+
 const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleLogin = () => {
-    alert("login");
+  const navigate = useNavigate();
+  const handleLogin = async () => {
+    //validate
+    //submit api
+    let data = await postLogin(email, password);
+    if (data && +data.EC === 0) {
+      toast.success(data.EM);
+      navigate("/");
+    }
+    if (data && +data.EC !== 0) {
+      toast.error(data.EM);
+    }
   };
   return (
     <div className="login-container">
-      <div className="header">Don't have an account yet?</div>
+      <div className="header">
+        <span>Don't have an account yet?</span>
+        <button>Sign up</button>
+      </div>
       <div className="title col-4 mx-auto">HoiDanIT</div>
       <div className="welcome col-4 mx-auto">Hello, who's this?</div>
       <div className="content-form col-4 mx-auto">
@@ -39,6 +56,17 @@ const Login = (props) => {
           <button className="btn-submit" onClick={() => handleLogin()}>
             Login to HoiDanIT
           </button>
+          <div className="text-center">
+            <span
+              className="back"
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              {" "}
+              &#60; &#60; Go to Homepage
+            </span>
+          </div>
         </div>
       </div>
     </div>
