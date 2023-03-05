0b367512568d62b643f79a5f089e4c2b84e5b3c1
HungNguyen-89 <hungnguyenhuts89@gmail.com>
2023-03-05 09:27:48 +0700

    video#88

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,17 +1,19 @@
 import { useState, useEffect } from "react";
 import { useParams, useLocation } from "react-router-dom";
-import { getDataQuiz } from "../../services/apiServices";
+import { getDataQuiz, postSubmitQuiz } from "../../services/apiServices";
 import _ from "lodash";
 import "./DetailQuiz.scss";
 import Question from "./Question";
+import ModalResult from "./ModalResult";
 
 const DetailQuiz = (props) => {
   const params = useParams();
   const location = useLocation();
   const quizId = params.id;
-
   const [dataQuiz, setDataQuiz] = useState([]);
   const [index, setIndex] = useState(0);
+  const [isShowModalResult, setIsShowModalResult] = useState(false);
+  const [dataModalResult, setDataModalResult] = useState({});
 
   useEffect(() => {
     fetchQuestions();
@@ -77,7 +79,7 @@ const DetailQuiz = (props) => {
     }
   };
 
-  const handleFinishQuiz = () => {
+  const handleFinishQuiz = async () => {
     console.log(">>> check data before submit: ", dataQuiz);
     let payload = {
       quizId: +quizId,
@@ -99,7 +101,19 @@ const DetailQuiz = (props) => {
         });
       });
       payload.answers = answers;
-      console.log("final payload: ", payload);
+      //submit api
+      let res = await postSubmitQuiz(payload);
+      console.log("check res: ", res);
+      if (res && res.EC === 0) {
+        setDataModalResult({
+          countCorrect: res.DT.countCorrect,
+          countTotal: res.DT.countTotal,
+          quizData: res.DT.quizData,
+        });
+        setIsShowModalResult(true);
+      } else {
+        alert("something wrongs...");
+      }
     }
   };
 
@@ -136,6 +150,11 @@ const DetailQuiz = (props) => {
         </div>
       </div>
       <div className="right-content">count down</div>
+      <ModalResult
+        show={isShowModalResult}
+        setShow={setIsShowModalResult}
+        dataModalResult={dataModalResult}
+      />
     </div>
   );
 };
--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -60,6 +60,10 @@ const getDataQuiz = (id) => {
   return axios.get(`/api/v1/questions-by-quiz?quizId=${id}`);
 };
 
+const postSubmitQuiz = (data) => {
+  return axios.post(`/api/v1/quiz-submit`, { ...data });
+};
+
 export {
   postCreateNewUser,
   getAllUsers,
@@ -70,4 +74,5 @@ export {
   postRegister,
   getQuizByUser,
   getDataQuiz,
+  postSubmitQuiz,
 };
